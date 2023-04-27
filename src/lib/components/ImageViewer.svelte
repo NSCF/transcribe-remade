2d9320acb65fc7eb354d05735842f9db59c30d02
Ian Engelbrecht <ianicus.za@gmail.com>
2023-04-27 18:13:29 +0200

    added multi-images

--- src/lib/components/ImageViewer.svelte
+++ src/lib/components/ImageViewer.svelte
@@ -2,13 +2,14 @@
   import {onMount} from 'svelte'
   import OpenSeaDragon from 'openseadragon'
 
-  export let image = 'PRU0006054.dzi'
+  export let images = []
   let viewer = null
 
   onMount(_ => {
+    let imageList = images.map(image => `/images/${image}`)
     viewer = OpenSeaDragon({
       id: "imageviewer",
-      tileSources: `/images/${image}`,
+      tileSources: imageList,
       constrainDuringPan: true,
       animationTime: 0,
       showNavigator: true,
@@ -18,9 +19,25 @@
     });
   })
 
-
-$: if (viewer && image) viewer.tileSources = `/images/${image}`
+  $: if (images.length) console.log('image', images[0])
   
 </script>
 
-<div id="imageviewer" style="width: 100%; height: 100%" />
+<div class="relative w-full, h-full">
+  <div id="imageviewer" style="width: 100%; height: 100%" />
+  {#if images.length > 1}
+  <div class="absolute bottom-0 w-full p-2 flex justify-center items-center gap-2" hidden={images.length == 1}>
+    <button class='btn' on:click={viewer.goToPreviousPage()}>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+      </svg>
+    </button>
+    <!-- <span class="p-2">{currentIndex + 1} of {images.length}</span> -->
+    <button class='btn' on:click={viewer.goToNextPage()}>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+      </svg>
+    </button>
+  </div>
+  {/if}
+</div>
--- src/routes/specimen/+page.svelte
+++ src/routes/specimen/+page.svelte
@@ -1,11 +1,44 @@
 <script>
   import ImageViewer from "$lib/components/ImageViewer.svelte";
   import DataForm from "$lib//components/DataForm.svelte";
+
+  let images = ['PRU0006054.dzi',
+    'PRU0038707.dzi',
+    'PRU0046633.dzi',
+    'PRU0046640.dzi',
+    'PRU0046648.dzi',
+    'PRU0047133.dzi',
+    'PRU0049434.dzi',
+    'PRU0050045.dzi',
+    'PRU0050378.dzi',
+    'PRU0050379.dzi',
+    'PRU0050412.dzi',
+    'PRU0059986.dzi',
+    'PRU0074608.dzi',
+    'PRU0094421.dzi',
+    'PRU0094421.dzi']
+    
+  let currentIndex = 0
+
+  const previousImage = _ => {
+    if(currentIndex > 0) {
+      currentIndex--
+    }
+  }
+
+  const nextImage = _ => {
+    if (currentIndex <= images.length - 1) {
+      currentIndex++
+    }
+  }
+
+  
+
 </script>
 
 <div class="grid w-full h-full grid-cols-3 gap-1">
   <div class="col-span-2 w-full h-full border rounded">
-    <ImageViewer />
+    <ImageViewer {images} />
   </div>
   <div class="h-full min-h-0  p-2 border rounded">
     <DataForm on:record-saved={console.log}/>
